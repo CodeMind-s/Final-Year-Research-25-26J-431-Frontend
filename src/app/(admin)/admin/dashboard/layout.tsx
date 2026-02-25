6db91f172a8c4931334585c79919434cb672b25b
arshartisan <arshartisan@gmail.com>
2026-02-25 14:01:45 +0530

    feat(AdminDashboard): add layout and overview page components for admin dashboard

---
+++ src/app/(admin)/admin/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { AdminDashboardLayout } from "@/components/admin/AdminDashboardLayout";
+import { ProtectedRoute } from "@/components/common/ProtectedRoute";
+import { UserRole } from "@/dtos/auth.dto";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ProtectedRoute
+      requiredRole={[UserRole.ADMIN, UserRole.SUPERADMIN]}
+      redirectTo="/auth/admin"
+    >
+      <AdminDashboardLayout>{children}</AdminDashboardLayout>
+    </ProtectedRoute>
+  );
+}
