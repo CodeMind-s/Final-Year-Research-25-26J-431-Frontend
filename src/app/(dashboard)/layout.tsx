164490c99eefba9dfefa7bf8e9ebd04756628352
bhashanasirimanna <bhashanasirimanna@gmail.com>
2026-03-01 22:36:59 +0530

    feat: update required roles in ProtectedRoute to include Landowner

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ import { UserRole } from "@/dtos/auth.dto";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ProtectedRoute
-      requiredRole={[UserRole.ADMIN, UserRole.SUPERADMIN, UserRole.SALTSOCIETY, UserRole.LABORATORY]}
+      requiredRole={[UserRole.ADMIN, UserRole.SUPERADMIN, UserRole.SALTSOCIETY, UserRole.LABORATORY, UserRole.LANDOWNER]}
       redirectTo="/auth/admin"
     >
       <AdminDashboardLayout>{children}</AdminDashboardLayout>
